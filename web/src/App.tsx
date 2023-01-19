3e42f5d96fd893dbaae1c776a4410ad589446ffc
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-18 21:28:00 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,10 @@ import './styles/global.css';
 export function App() {
 
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+
+      </div>
       
     </div>
   )
