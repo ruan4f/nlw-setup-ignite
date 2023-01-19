5f2b3892a2ae369e4842c89b06b7cd042a725fc5
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-01-19 20:20:33 -0300

    Tailwind configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,8 +6,9 @@ import {
   useFonts
 } from '@expo-google-fonts/inter';
 
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,22 +23,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+    <>
+      <Home/>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
